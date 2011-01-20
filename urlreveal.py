0cfb08f26820f644a76cccd186534b5b81883ea2
Patrick Mylund Nielsen <patrick@patrickmylund.com>
2011-01-20 23:28:16 +0100

    google analytics tag

--- urlreveal.py
+++ urlreveal.py
@@ -17,6 +17,17 @@ header = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www
 <meta name="keywords" content="urlreveal, url reveal, url,redirect,url redirect" />
 <link rel="canonical" href="http://urlreveal.tmwiw.com/" />
 <!-- <link rel="SHORTCUT ICON" href="http://cdn.pmylund.com/favicon.ico" /> -->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-270876-26']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 </head>
 <body><center>
     <h1>UrlReveal</h1>
